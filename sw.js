76a097ad0bdca9dc1c0f9e4116211eb3dca1bbeb
Sogo Ohta <sogo.ohta@eure.jp>
2016-07-26 15:24:24 +0900

    Add

---
+++ sw.js
@@ -0,0 +1,20 @@
+console.log('Started', self);
+
+self.addEventListener('install', function(event) {
+  self.skipWaiting();
+  console.log('Installed', event);
+});
+
+self.addEventListener('activate', function(event) {
+  console.log('Activated', event);
+});
+
+self.addEventListener('push', function(event) {
+  console.log('Push message received', event);
+  // TODO
+});
+
+self.addEventListener('fetch', function(event) {
+  console.log('fetch!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!);
+  console.log(event.request.url);
+});
